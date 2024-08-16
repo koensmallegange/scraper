292fdccb898824026a3b03ac19e187866a0b9884
koensmallegange <koen.smallegange@live.nl>
2024-08-16 20:05:47 +0200

    Added dependency management

--- scraper/main.py
+++ scraper/main.py
@@ -1,24 +1,27 @@
-'''Run this script to activate the scraper'''
-
 from controller import RealEstateController
 from model import RealEstateModel
 import yaml
 
 # Load the config.yaml file
-config_path = '../config.yaml'  # Adjust the path as necessary
+config_path = 'config.yaml'  
+secrets_path = 'secrets.yaml'  
 
-# Extacht URL and email from config
+# Extract URL and email from config
 with open(config_path, 'r') as file:
     config = yaml.safe_load(file)
 
-# Extract the URL and email credentials from the config
+# Extract the password from secrets.yaml
+with open(secrets_path, 'r') as file:
+    secrets = yaml.safe_load(file)
+
+# Extract the URL and email credentials from the config and secrets
 url = config['url']
 email_credentials = {
     'from': config['email']['from'],
     'to': config['email']['to'],
     'smtp_server': config['email']['smtp_server'],
     'smtp_port': config['email']['smtp_port'],
-    'password': config['email']['password'],
+    'password': secrets['email']['password'],
 }
 
 # Create the Model
